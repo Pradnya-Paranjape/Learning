cefddba31c77045753e7ac1797f6f58ba895dd15
Pradnya-Paranjape <pradnya.paranjape@gmail.com>
2014-10-28 17:52:10 +0530

    added the send mail functionality which sends mail locally for now

--- index.js
+++ index.js
@@ -13,22 +13,36 @@ var register = require("./lib/registration.js");
 app.set("port",process.env.port||3000);
 
 function start(){
-app.listen(app.get("port"),function(res,rep){
-	console.log("Server started");
-	app.post("/register",function(req,res){
-	var reg = new register(req.body,app);
-	reg.init();
-	var ret = reg.insert();
-	if(ret == false){console.log("Cannot Proceed further...")}
-	else{
-	eventEmitter.on("inserted",callMail);
-	function callMail(){
-		reg.sendMail();
-	}
-	eventEmitter.on("mlsnt",reg.sucess);
-	}	
-});
-});
+	app.listen(app.get("port"),function(res,rep){
+		console.log("Server started");
+		app.post("/register",function(req,res){
+			res.setHeader('Content-Type', 'application/json');
+			var reg = new register(req.body,app);
+			reg.init();
+			var ret = reg.insert();
+			if(ret == false){
+				var err = app.get("error");
+				var jsonErr = {};
+				jsonErr.error = err;
+				res.end(JSON.stringify(jsonErr));
+			}
+			else{
+				eventEmitter.on("inserted",callMail);
+				function callMail(){
+					reg.sendMail();
+					eventEmitter.on("mailSentEvnt",function(){
+						infor = {'info':'Mail Sent sucessfully!!'};
+						res.end(JSON.stringify(infor));
+					});
+					eventEmitter.on("mailNtSentEvnt",function(){
+						infor = {'info':'Mail Not Sent'};
+						res.end(JSON.stringify(infor));
+					});
+				}
+				eventEmitter.on("mlsnt",reg.sucess);
+			}	
+		});
+	});
 }
 
 eventEmitter.on("appset",start);
